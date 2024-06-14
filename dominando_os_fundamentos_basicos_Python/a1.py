51807c08a2f98b28f03bf72139db67833646a71e
Cristina Ramos <60412708+cristinards@users.noreply.github.com>
2024-06-14 17:47:59 -0300

    Adicionando os códigos
    Adicionando códigos do Desafio de Código: Dominando os Fundamentos Básicos do Python

---
+++ dominando_os_fundamentos_basicos_Python/a1.py
@@ -0,0 +1,17 @@
+# Crie uma Função: recomendar_plano para receber o consumo médio mensal:
+def recomendar_plano(consumo):
+    
+    # Crie uma Estrutura Condicional para verifica o consumo médio mensal 
+    if consumo <= 10:
+        return "Plano Essencial Fibra - 50Mbps"
+    elif consumo <= 20:
+        return "Plano Prata Fibra - 100Mbps"
+    else:
+        return "Plano Premium Fibra - 300Mbps"
+
+# Retorne o plano de internet adequado:
+
+# Solicita ao usuário que insira o consumo médio mensal de dados:
+consumo = float(input())
+# Chama a função recomendar_plano com o consumo inserido e imprime o plano recomendado:
+print(recomendar_plano(consumo))
